48ab5ef2fcd5af6229168c6c10ab1c46e63d15a7
AymericN <ay.neumann@laposte.net>
2020-03-21 12:16:00 +0100

    First Commit

---
+++ Aapi/src/main/java/com/example/Aapi/AapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Aapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AapiApplication.class, args);
+	}
+
+}
---
+++ Aapi/src/test/java/com/example/Aapi/AapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
