749c4351f0827ff384e3fd1afdf2fcaa53898ad2
AymericN <ay.neumann@laposte.net>
2020-04-14 21:45:25 +0200

    ADD FEAT BLOBJ NAME CONSTRAINT
    Change method name formatBlobJData to checkAndFormatBlobJData in
    BlobJService.

--- Aapi/src/main/java/com/example/Aapi/cron/DatabaseCleaner.java
+++ Aapi/src/main/java/com/example/Aapi/cron/DatabaseCleaner.java
@@ -1,5 +1,7 @@
 package com.example.Aapi.cron;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +30,7 @@ public class DatabaseCleaner {
 	/**
 	 * Delete all BlobJ without a sign.
 	 */
+	@PostConstruct
 	@Scheduled(cron = "0 */10 * * * *")
 	public void deleteBlobJsWithoutSign() {
 		Integer pageNumber = 0;
